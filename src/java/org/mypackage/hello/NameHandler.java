6dd3b2a4cdf37bc5f27eb6796f4bb53430bf0106
danielisangedighi <danielisangedighi@outlook.com>
2023-12-16 22:11:58 +0100

    Completion 10:13 16122023

---
+++ src/java/org/mypackage/hello/NameHandler.java
@@ -0,0 +1,36 @@
+package org.mypackage.hello;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Daniel
+ */
+public class NameHandler {
+    private String name;
+
+    /** Creates a new instance of NameHandler */
+    public NameHandler() {
+       name = null;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+    
+}
--- web/index.jsp
+++ web/index.jsp
@@ -12,6 +12,12 @@
         <title>JSP Page</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1>Entry Form</h1>
+        
+        <form name="Name Input Form" action="response.jsp">
+            Enter your name:
+            <input type="text" name="name" value="" />
+            <input type="submit" value="OK" name="submit" />
+        </form>
     </body>
 </html>
